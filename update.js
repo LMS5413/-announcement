6d9ab7e61264809b17b97b72dd378ea3e43b9c12
Leonardo Machado <machadodasilvaleonardo@gmail.com>
2020-11-24 09:55:23 -0300

    annoucenter

---
+++ update.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require('discord.js')
+
+exports.run = (client, message, args) => {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return;
+
+    let canal =  message.guild.channels.cache.get("channel id")
+    let anuncio1 = args[0]
+    let anuncio2 = args[1]
+    if(!anuncio1) return message.reply("Diga o anuncio 1")
+    if(!anuncio2) return message.reply("Diga o anuncio 2")
+
+    const embed = new MessageEmbed()
+        .setTitle("UPDATE DO BOT")
+        .addField("Versão da UPDATE", `${anuncio1}`)
+        .addField("UPDATE feito", `${anuncio2}`)
+    canal.send('@everyone').then(deletar => {
+        deletar.delete({timeout: 1000})
+    })
+    canal.send(embed)
+}
+exports.help = {
+    name: "update"
+}
